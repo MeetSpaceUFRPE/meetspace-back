c90b05e7b2f91c66438c3b7a5d27818b07e83e61
Lucas Matias <lucasmatiassilva2022@gmail.com>
2025-01-25 12:12:25 -0300

    refactor: um monte de coisa aff jeane

--- services/reservation-service/index.js
+++ services/reservation-service/index.js
@@ -1,18 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./src/config/dbConfig.js";
+import { connectDB, sequelize } from "./src/config/dbConfig.js";
 import reservationRoutes from "./src/routes/reservationRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const PORTA = process.env.PORT || 3000; 
+const PORTA = process.env.PORT || 3005;
 
 app.use(cors());
 app.use(express.json());
 
-connectDB();
+await connectDB();
+
+// Sincroniza modelos com o banco de dados
+sequelize.sync({ alter: true })
+  .then(() => console.log("Modelos sincronizados com o banco de dados."))
+  .catch((error) => console.error("Erro ao sincronizar modelos:", error.message));
 
 // Rotas de reservas
 app.use("/api/reservas", reservationRoutes);
--- services/reservation-service/src/controllers/reservationController.js
+++ services/reservation-service/src/controllers/reservationController.js
@@ -12,13 +12,9 @@ export const createReservation = async (req, res) => {
 
 export const getReservations = async (req, res) => {
   try {
-    const reservations = await Reservation.find()
-      .populate("usuarioId", "nome")  // Popula os dados do usuário (ajuste conforme necessário)
-      .populate("salaId", "nome");    // Popula os dados da sala (ajuste conforme necessário)
+    const reservations = await Reservation.findAll();
     res.status(200).json(reservations);
   } catch (error) {
     res.status(500).json({ error: "Erro ao buscar reservas: " + error.message });
   }
 };
-
-// Adicione mais métodos conforme necessário (update, delete, etc.)
