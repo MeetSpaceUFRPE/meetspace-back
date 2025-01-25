0ff130ea70453aa4bdc0d80d428855985ae2c3e6
brenno-araujo25 <brennocaldeira@outlook.com>
2025-01-25 19:59:09 -0300

    feat: add reservation service Docker support and environment configuration

--- services/sala-service/src/config/dbConfig.js
+++ services/sala-service/src/config/dbConfig.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   dialect: 'postgres',
   logging: false, // Desativa logs no console
 });
