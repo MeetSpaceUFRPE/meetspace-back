d9abdc03e2be14425ccc79a7f2208272cf5638e8
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-02-19 14:34:49 -0300

    feat: adião de arquivo tsconfig e tsconfig.build

--- services/calendar-service/index.js
+++ services/calendar-service/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { sequelize } from './src/config/dbConfig.js';
-import calendarRoutes from './src/routes/calendarRoutes.js'
+import calendarRoutes from './src/routes/calendarRoutes.js';
 
 dotenv.config();
 
@@ -11,17 +10,6 @@ const PORT = process.env.CALENDAR_SERVICE_PORT || 3007;
 
 app.use(cors());
 app.use(express.json());
-
-// Conectar ao banco de dados
-sequelize.authenticate()
-  .then(() => {
-    console.log('PostgreSQL conectado com sucesso.');
-    sequelize.sync({ alter: true });
-  })
-  .catch((error) => {
-    console.error('Erro ao conectar ao PostgreSQL:', error.message);
-    process.exit(1);
-  });
 
 // Rotas
 app.use('/api/calendar', calendarRoutes);
